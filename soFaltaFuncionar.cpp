e4739c39306585059773460f19c830c339a5e4c2
noc1243 <noc1243@users.noreply.github.com>
2016-07-13 14:57:39 -0300

    Except for some exceptional phases and the step end it fucking WORKS!!

--- soFaltaFuncionar.cpp
+++ soFaltaFuncionar.cpp
@@ -93,7 +93,7 @@ elemento netlist[MAX_ELEM]; /* Netlist */
 struct cmd
 {
   char modo[MAX_NOME]; //tipo de escala (linear, octal ou decada)
-  int ptos, ptInicio, ptFim; //ponto inicial, ponto final e o numero de pontos (total se for linear, por decada)
+  double ptos, ptInicio, ptFim; //ponto inicial, ponto final e o numero de pontos (total se for linear, por decada)
   bool temComando; //indica se ha linha de comando
 } comando;
 
@@ -264,13 +264,13 @@ int main(void)
     }
     else if (tipo=='.')
     {
-      sscanf(p,"%10s%d%d%d",comando.modo, &comando.ptos, &comando.ptInicio, &comando.ptFim);
+      sscanf(p,"%10s%lg%lg%lg",comando.modo, &comando.ptos, &comando.ptInicio, &comando.ptFim);
       comando.temComando = true;
       printf("\n\n\n");
       printf("Modo: %-s\n", comando.modo);
-      printf("Pontos: %d\n", comando.ptos);
-      printf("Inicio: %d\n", comando.ptInicio);
-      printf("Fim: %d\n\n", comando.ptFim);
+      printf("Pontos: %lg\n", comando.ptos);
+      printf("Inicio: %lg\n", comando.ptInicio);
+      printf("Fim: %lg\n\n", comando.ptFim);
       if (modoExib != mostraResultado)
         getch();
       ne--;
@@ -445,7 +445,7 @@ int main(void)
     {
       double passo = 1.0/ (comando.ptos - 1);
       double freq = 0.0;
-      for (freq = comando.ptInicio; freq <= comando.ptFim; freq *= pow(10,1.0/(comando.ptos - 1)) )
+      for (freq = comando.ptInicio; freq <= comando.ptFim; freq *= pow(10,1.0/(comando.ptos - 1)))
       {
           if (modoExib == mostraTudo)
           {
@@ -493,6 +493,8 @@ int main(void)
           nPtos++;
           if (modoExib == mostraTudo)
             getch();
+          if (freq * pow(10,1.0/(comando.ptos - 1)) > comando.ptFim && freq !=comando.ptFim)
+        	  freq = comando.ptFim/pow(10,1.0/(comando.ptos - 1));
         }
         printf("Foram plotados %d pontos\n", nPtos);
     }
@@ -547,6 +549,8 @@ int main(void)
           nPtos++;
           printf("\nFrequencia: %f\n", freq);
           getch();
+          if (freq * pow(2,1.0/(comando.ptos - 1)) > comando.ptFim && freq !=comando.ptFim)
+        	  freq = comando.ptFim/pow(2,1.0/(comando.ptos - 1));
         }
         printf("Foram plotados %d pontos\n", nPtos);
     }
@@ -646,16 +650,16 @@ int resolversistema(void)
     if (fabs(t)<TOLGDC) {
       //printf("Sistema singular\n");
       //printf ("%lf\n", (fabs(t) - TOLG)*1000000000);
-    	if(!linear)
-    	{
+//    	if(!linear)
+//    	{
     		trocaValores = true;
     		return 0;
-    	}
-    	else
-    	{
-    		printf("Sistema singular\n");
-    		return 1;
-    	}
+//    	}
+//    	else
+//    	{
+//    		printf("Sistema singular\n");
+//    		return 1;
+//    	}
     }
     for (j=nv+1; j>0; j--) {  /* Basta j>i em vez de j>0 */
       Yn[i][j]/= t;
@@ -949,140 +953,143 @@ void montaEstampaDC()
        Yn[netlist[i].x][netlist[i].x]+= g;
     }
 
-   else if (tipo=='M')
-     {
-          g =  FATORDC;
-          netlist[i].gm = 0.0;
-          netlist[i].gds = 0.0;
-          netlist[i].gmb = 0.0;
-          io = 0.0;
+    else if (tipo=='M')
+         {
+              g =  FATORDC;
+              netlist[i].gm = 0.0;
+              netlist[i].gds = 0.0;
+              netlist[i].gmb = 0.0;
+              io = 0.0;
 
-          double vds = vAtual[netlist[i].td]-vAtual[netlist[i].ts];
+              double vds = vAtual[netlist[i].td]-vAtual[netlist[i].ts];
 
-          if ((vds < 0 && netlist[i].pnmos == nmos) || (vds > 0 && netlist[i].pnmos == pmos))
-          {
-           	  int aux  = netlist[i].td;
-           	  netlist[i].td = netlist[i].ts;
-           	  netlist[i].ts = aux;
-          }
+              if ((vds < 0 && netlist[i].pnmos == nmos) || (vds > 0 && netlist[i].pnmos == pmos))
+              {
+               	  int aux  = netlist[i].td;
+               	  netlist[i].td = netlist[i].ts;
+               	  netlist[i].ts = aux;
+              }
 
-		  #ifdef DEBUG
-          	  //printf ("vd %f vs %f vb %f vg %f\n", vAtual[netlist[i].td],vAtual[netlist[i].ts], vAtual[netlist[i].tb], vAtual[netlist[i].tg]);
-		  //getch();
-		  #endif
+    		  #ifdef DEBUG
+              	  //printf ("vd %f vs %f vb %f vg %f\n", vAtual[netlist[i].td],vAtual[netlist[i].ts], vAtual[netlist[i].tb], vAtual[netlist[i].tg]);
+    		  //getch();
+    		  #endif
 
-          double vgs = vAtual[netlist[i].tg]-vAtual[netlist[i].ts];
-          vds = vAtual[netlist[i].td]-vAtual[netlist[i].ts];
-          double vbs = vAtual[netlist[i].tb]-vAtual[netlist[i].ts];
+              double vgs = vAtual[netlist[i].tg]-vAtual[netlist[i].ts];
+              vds = vAtual[netlist[i].td]-vAtual[netlist[i].ts];
+              double vbs = vAtual[netlist[i].tb]-vAtual[netlist[i].ts];
 
-          vgs *= (netlist[i].pnmos==nmos?1.0:-1.0);
-          vds *= (netlist[i].pnmos==nmos?1.0:-1.0);
+              vgs *= (netlist[i].pnmos==nmos?1.0:-1.0);
+              vds *= (netlist[i].pnmos==nmos?1.0:-1.0);
+              vbs *= (netlist[i].pnmos==nmos?1.0:-1.0);
 
-          vbs = (fabs(vbs)>netlist[i].phi/2.0?netlist[i].phi/2.0:vbs);
-
-          double vt = netlist[i].vt + netlist[i].gama * (sqrt((netlist[i].phi - fabs(vbs))) - sqrt((netlist[i].phi)));
-          //double vt = netlist[i].vt + netlist[i].gama * (sqrt((netlist[i].phi - vbs)) - sqrt((netlist[i].phi)));
-
-          vbs *= (netlist[i].pnmos==nmos?1.0:-1.0);
-
-          #ifdef DEBUG
-          	  printf ("vt: %f  vgs: %f vds: %f vs: %f ts %d\n", vt,vgs,vds, vAtual[netlist[i].ts], netlist[i].ts);
-		  #endif
+              vbs = (vbs>netlist[i].phi/2.0?netlist[i].phi/2.0:vbs);
 
 
-          if (vgs < vt)
-          {
-               netlist[i].transistorOp = corte;
-			   #ifdef DEBUG
-               	   printf ("corte\n");
-			   #endif
-          }
-          else if (vds < vgs - vt)
-          {
-        	   netlist[i].transistorOp = ohmica;
-			   #ifdef DEBUG
-               	   printf ("ohmica\n");
-			   #endif
-          }
-          else
-          {
-        	  netlist[i].transistorOp = saturacao;
-			  #ifdef DEBUG
-              	  printf ("saturacao\n");
-			  #endif
-          }
 
-          if (netlist[i].transistorOp == saturacao)
-          {
-        	  netlist[i].gm = netlist[i].k * (netlist[i].w/netlist[i].l)* (2.0*(vgs - vt)) * (1 + netlist[i].lambda* vds);
-        	  netlist[i].gds = netlist[i].k * (netlist[i].w/netlist[i].l)* pow ((vgs - vt),2.0) * netlist[i].lambda;
-            io = netlist[i].k * (netlist[i].w/netlist[i].l) * pow((vgs - vt),2.0) * (1.0 + netlist[i].lambda * vds) - (netlist[i].gm * vgs) - (netlist[i].gds * vds);
-          }
-          else if (netlist[i].transistorOp == ohmica)
-          {
-        	  netlist[i].gm = netlist[i].k * (netlist[i].w/netlist[i].l)*(2.0* vds)*(1+ netlist[i].lambda* vds);
-        	  netlist[i].gds = netlist[i].k * (netlist[i].w/netlist[i].l) * (2.0*(vgs - vt) - 2 * vds + 4.0* netlist[i].lambda * (vgs - vt) * vds - 3.0*netlist[i].lambda * pow (vds,2.0));
-            io = netlist[i].k * (netlist[i].w/netlist[i].l) * (2.0* (vgs - vt)*vds - pow (vds,2.0)) - (netlist[i].gm * vgs) - (netlist[i].gds * vds);
-          }
+              double vt = netlist[i].vt + netlist[i].gama * (sqrt((netlist[i].phi - vbs)) - sqrt((netlist[i].phi)));
+              //double vt = netlist[i].vt + netlist[i].gama * (sqrt((netlist[i].phi - vbs)) - sqrt((netlist[i].phi)));
 
-          if (gm != 0.0)
-            cout << "gm: " << gm << endl;
-          //Q?
-          //netlist[i].gmb = (netlist[i].gm*netlist[i].gama)/(sqrt(fabs(netlist[i].phi - (vAtual[netlist[i].tb] -vAtual[netlist[i].ts]))));
-          //netlist[i].gmb = (netlist[i].gm*netlist[i].gama)/(2*sqrt(fabs(netlist[i].phi - (vAtual[netlist[i].tb] -vAtual[netlist[i].ts]))));
-          //netlist[i].gmb = (netlist[i].gm*netlist[i].gama)/(2*sqrt(fabs(netlist[i].phi - (vbs) ) ));
-
-          //GM ESTA IGUAL A 0.0 E FUNCIONA WTF?
-          netlist[i].gmb = (netlist[i].gm*netlist[i].gama)/(2*sqrt(fabs(netlist[i].phi - (vbs) ) ));
-
-          //io-= 0.0;
-          //io-= (netlist[i].gmb * vbs);
-          io*=(netlist[i].pnmos == pmos?-1.0:1.0);
-
-          tvgs = vgs;
-          tvds = vds;
-          tvbs = vbs;
-          //ti0 = netlist[i].k * (netlist[i].w/netlist[i].l) * (2.0* (vgs - vt)*vds - pow (vds,2.0)) * (1.0 + netlist[i].lambda * vds);
-          ti0 = netlist[i].k * (netlist[i].w/netlist[i].l) * pow((vgs - vt),2.0) * (1.0 + netlist[i].lambda * vds);
-	   	 #ifdef DEBUG
-		 	   //printf ("gm %e gmb %e  gds %e io %e \n\n", gm, gmb, gds, io);
-		 #endif
-
-          Yn[netlist[i].td][netlist[i].tb]+=netlist[i].gmb;
-          Yn[netlist[i].ts][netlist[i].ts]+=netlist[i].gmb;
-          Yn[netlist[i].td][netlist[i].ts]-=netlist[i].gmb;
-          Yn[netlist[i].ts][netlist[i].tb]-=netlist[i].gmb;
-
-          Yn[netlist[i].td][netlist[i].tg]+=netlist[i].gm;
-          Yn[netlist[i].ts][netlist[i].ts]+=netlist[i].gm;
-          Yn[netlist[i].td][netlist[i].ts]-=netlist[i].gm;
-          Yn[netlist[i].ts][netlist[i].tg]-=netlist[i].gm;
-
-          Yn[netlist[i].td][netlist[i].td]+=netlist[i].gds;
-          Yn[netlist[i].ts][netlist[i].ts]+=netlist[i].gds;
-          Yn[netlist[i].td][netlist[i].ts]-=netlist[i].gds;
-          Yn[netlist[i].ts][netlist[i].td]-=netlist[i].gds;
+              #ifdef DEBUG
+              	  printf ("vt: %f  vgs: %f vds: %f vs: %f ts %d\n", vt,vgs,vds, vAtual[netlist[i].ts], netlist[i].ts);
+    		  #endif
 
 
-         Yn[netlist[i].td][netlist[i].td]+=g;
-         Yn[netlist[i].tg][netlist[i].tg]+=g;
-         Yn[netlist[i].td][netlist[i].tg]-=g;
-         Yn[netlist[i].tg][netlist[i].td]-=g;
+              if (vgs < vt)
+              {
+                   netlist[i].transistorOp = corte;
+    			   #ifdef DEBUG
+                   	   printf ("corte\n");
+    			   #endif
+              }
+              else if (vds < vgs - vt)
+              {
+            	   netlist[i].transistorOp = ohmica;
+    			   #ifdef DEBUG
+                   	   printf ("ohmica\n");
+    			   #endif
+              }
+              else
+              {
+            	  netlist[i].transistorOp = saturacao;
+    			  #ifdef DEBUG
+                  	  printf ("saturacao\n");
+    			  #endif
+              }
 
-         Yn[netlist[i].ts][netlist[i].ts]+=g;
-         Yn[netlist[i].tg][netlist[i].tg]+=g;
-         Yn[netlist[i].ts][netlist[i].tg]-=g;
-         Yn[netlist[i].tg][netlist[i].ts]-=g;
+              if (netlist[i].transistorOp == saturacao)
+              {
+            	  netlist[i].gm = netlist[i].k * (netlist[i].w/netlist[i].l)* (2.0*(vgs - vt)) * (1 + netlist[i].lambda* vds);
+            	  netlist[i].gds = netlist[i].k * (netlist[i].w/netlist[i].l)* pow ((vgs - vt),2.0) * netlist[i].lambda;
+                io = netlist[i].k * (netlist[i].w/netlist[i].l) * pow((vgs - vt),2.0) * (1.0 + netlist[i].lambda * vds) - (netlist[i].gm * vgs) - (netlist[i].gds * vds);
+              }
+              else if (netlist[i].transistorOp == ohmica)
+              {
+            	  netlist[i].gm = netlist[i].k * (netlist[i].w/netlist[i].l)*(2.0* vds)*(1+ netlist[i].lambda* vds);
+            	  netlist[i].gds = netlist[i].k * (netlist[i].w/netlist[i].l) * (2.0*(vgs - vt) - 2 * vds + 4.0* netlist[i].lambda * (vgs - vt) * vds - 3.0*netlist[i].lambda * pow (vds,2.0));
+                io = netlist[i].k * (netlist[i].w/netlist[i].l) * (2.0* (vgs - vt)*vds - pow (vds,2.0)) - (netlist[i].gm * vgs) - (netlist[i].gds * vds);
+              }
 
-         Yn[netlist[i].tb][netlist[i].tb]+=g;
-         Yn[netlist[i].tg][netlist[i].tg]+=g;
-         Yn[netlist[i].tb][netlist[i].tg]-=g;
-         Yn[netlist[i].tg][netlist[i].tb]-=g;
+              if (gm != 0.0)
+                cout << "gm: " << gm << endl;
+              //Q?
+              //netlist[i].gmb = (netlist[i].gm*netlist[i].gama)/(sqrt(fabs(netlist[i].phi - (vAtual[netlist[i].tb] -vAtual[netlist[i].ts]))));
+              //netlist[i].gmb = (netlist[i].gm*netlist[i].gama)/(2*sqrt(fabs(netlist[i].phi - (vAtual[netlist[i].tb] -vAtual[netlist[i].ts]))));
+              //netlist[i].gmb = (netlist[i].gm*netlist[i].gama)/(2*sqrt(fabs(netlist[i].phi - (vbs) ) ));
 
-          Yn[netlist[i].td][nv+1]-=io;
-          Yn[netlist[i].ts][nv+1]+=io;
+              //GM ESTA IGUAL A 0.0 E FUNCIONA WTF?
+              netlist[i].gmb = (netlist[i].gm*netlist[i].gama)/(2*sqrt(fabs(netlist[i].phi - (vbs) ) ));
 
-     }
+              //io-= 0.0;
+              io-= (netlist[i].gmb * vbs);
+              io*=(netlist[i].pnmos == pmos?-1.0:1.0);
+
+              tvgs = vgs;
+              tvds = vds;
+              tvbs = vbs;
+              //netlist[i].vds = vds;
+              //netlist[i].vbs = vAtual[netlist[i].tb]-vAtual[netlist[i].ts];//vbs;
+              //ti0 = netlist[i].k * (netlist[i].w/netlist[i].l) * (2.0* (vgs - vt)*vds - pow (vds,2.0)) * (1.0 + netlist[i].lambda * vds);
+              ti0 = netlist[i].k * (netlist[i].w/netlist[i].l) * pow((vgs - vt),2.0) * (1.0 + netlist[i].lambda * vds);
+    	   	 #ifdef DEBUG
+    		 	   //printf ("gm %e gmb %e  gds %e io %e \n\n", gm, gmb, gds, io);
+    		 #endif
+
+              Yn[netlist[i].td][netlist[i].tb]+=netlist[i].gmb;
+              Yn[netlist[i].ts][netlist[i].ts]+=netlist[i].gmb;
+              Yn[netlist[i].td][netlist[i].ts]-=netlist[i].gmb;
+              Yn[netlist[i].ts][netlist[i].tb]-=netlist[i].gmb;
+
+              Yn[netlist[i].td][netlist[i].tg]+=netlist[i].gm;
+              Yn[netlist[i].ts][netlist[i].ts]+=netlist[i].gm;
+              Yn[netlist[i].td][netlist[i].ts]-=netlist[i].gm;
+              Yn[netlist[i].ts][netlist[i].tg]-=netlist[i].gm;
+
+              Yn[netlist[i].td][netlist[i].td]+=netlist[i].gds;
+              Yn[netlist[i].ts][netlist[i].ts]+=netlist[i].gds;
+              Yn[netlist[i].td][netlist[i].ts]-=netlist[i].gds;
+              Yn[netlist[i].ts][netlist[i].td]-=netlist[i].gds;
+
+
+             Yn[netlist[i].td][netlist[i].td]+=g;
+             Yn[netlist[i].tg][netlist[i].tg]+=g;
+             Yn[netlist[i].td][netlist[i].tg]-=g;
+             Yn[netlist[i].tg][netlist[i].td]-=g;
+
+             Yn[netlist[i].ts][netlist[i].ts]+=g;
+             Yn[netlist[i].tg][netlist[i].tg]+=g;
+             Yn[netlist[i].ts][netlist[i].tg]-=g;
+             Yn[netlist[i].tg][netlist[i].ts]-=g;
+
+             Yn[netlist[i].tb][netlist[i].tb]+=g;
+             Yn[netlist[i].tg][netlist[i].tg]+=g;
+             Yn[netlist[i].tb][netlist[i].tg]-=g;
+             Yn[netlist[i].tg][netlist[i].tb]-=g;
+
+              Yn[netlist[i].td][nv+1]-=io;
+              Yn[netlist[i].ts][nv+1]+=io;
+
+         }
     else if (tipo=='K'){
           continue;
     }
